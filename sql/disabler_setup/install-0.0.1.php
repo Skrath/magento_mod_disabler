b7cc61f2622685d8a52706e8b65e3e09320ce7dc
Chris Rasys <chris.rasys@blueacorn.com>
2013-07-05 17:20:43 -0400

    Initial install script for module

---
+++ sql/disabler_setup/install-0.0.1.php
@@ -0,0 +1,41 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+$table = $installer->getConnection()->newTable($installer->getTable('disabler/segmentdisable'))
+                   ->addColumn('segment_disable_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+                       'unsigned' => true,
+                       'nullable' => false,
+                       'primary' => true,
+                       'identity' => true,
+                   ), 'Segment_disable ID')
+                   ->addColumn('segment_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+                       'unsigned' => true,
+                       'nullable' => false,
+                       'primary' => false,
+                       'identity' => false,
+                   ), 'Segment ID')
+                   ->addColumn('module_name', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+                       'nullable' => false,
+                   ), 'Module Name')
+                   ->addForeignKey('segment_key', 'segment_id', $installer->getTable('enterprise_customersegment/segment'), 'segment_id' );
+/*     ->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, null, array( */
+/*         'nullable' => false, */
+/*         ), 'Blogpost Title') */
+/*     ->addColumn('post', Varien_Db_Ddl_Table::TYPE_TEXT, null, array( */
+/*         'nullable' => true, */
+/*         ), 'Blogpost Body') */
+/*     ->addColumn('date', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array( */
+/*         ), 'Blogpost Date') */
+/*     ->addColumn('timestamp', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array( */
+/*         ), 'Timestamp') */
+/*     ->setComment('Magentotutorial weblog/blogpost entity table'); */
+$installer->getConnection()->createTable($table);
+
+// This is for testing purposes
+/* $segment_disable = Mage::getModel('disabler/segmentdisable'); */
+/* $blogpost->setSegmentId(1); */
+/* $blogpost->setModuleName('BlueAcorn_Obvious'); */
+/* $blogpost->save(); */
+
+$installer->endSetup();
