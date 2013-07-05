c08bdeace8a28acb4fb965cba6d2e631c34e22ab
Chris Rasys <chris.rasys@blueacorn.com>
2013-07-05 17:16:05 -0400

    Add function for disabling a module

--- Model/Observer.php
+++ Model/Observer.php
@@ -3,7 +3,21 @@
 class BlueAcorn_Disabler_Model_Observer {
 
         public function disableModules(Varien_Event_Observer $observer) {
+            /* $this->_disableModule('BlueAcorn_Obvious'); */
+        }
 
+        protected function _disableModule($moduleName) {
+            // Disable the module itself
+            $nodePath = "modules/$moduleName/active";
+            if (Mage::helper('core/data')->isModuleEnabled($moduleName)) {
+                Mage::getConfig()->setNode($nodePath, 'false', true);
+            }
+
+            // Disable its output as well (which was already loaded)
+            $outputPath = "advanced/modules_disable_output/$moduleName";
+            if (!Mage::getStoreConfig($outputPath)) {
+                Mage::app()->getStore()->setConfig($outputPath, true);
+            }
         }
 
 }
